80ce24a1f12cb45b38921db4a670c7440c647e31
RavshanSean <ravshansean1995@gmail.com>
2026-01-17 17:08:55 -0500

    private

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,25 @@
-console.log("kaka is working");
+import express from 'express';
+import cors from 'cors';
 
-const num1: number = 5;
-const num2: number = 10;
+import { ENV } from './config/env';
+import { clerkMiddleware } from '@clerk/express'
 
+const app = express();
 
-console.log(num1 + num2);
+app.use(cors({ origin: ENV.FRONTEND_URL })); //frontend run on 3000 port
+app.use(clerkMiddleware()); // auth obj will be attached to the req
+app.use(express.json()); //parese json req body
+app.use(express.urlencoded({ extended: true })); //parses form sata something like HTML forms
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcom to prouct API - Powered by PostgereSQL, Drizzle ORM & Clerk Auth',
+    endpoint: {
+        users: "/api/users",
+        products: "/api/products",
+        comments: "/api/comments",
+    },
+  });
+});
+
+app.listen(ENV.PORT, () => console.log('Server is running on PORT:', ENV.PORT));
